aef97d1714f5208b31bc8bab633fef622459ddef
DroppingAnvil <dr0pping.4nvi1@gmail.com>
2021-09-18 18:38:29 -0500

    Initial Commit

---
+++ MessageX/MessageXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageXApp.swift
+//  MessageX
+//
+//  Created by Dropping Anvil on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MessageXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
